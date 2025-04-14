80c0ac4f5705dc32860d7855617d21966c19793e
Joydip <joydip.mondal@gmail.com>
2025-04-14 13:20:12 +0530

    pre-lunch day-1 upload

---
+++ codes/day-1/FirstApp/FirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sample //name is NOT equal to the app name
+{
+    internal class Program
+    {
+        //static void Main()
+        //static void Main(string[] args)
+        //static int Main(){return 0;}
+        //static int Main(string[] args){return 0;}
+        static void Main()
+        {
+            Console.WriteLine("Hello to .NET and C#!");
+            Console.WriteLine("press any key to close the app: ");
+            Console.ReadKey();
+        }
+    }
+}
