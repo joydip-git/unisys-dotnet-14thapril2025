aa20a5c436700dac720d12bd7dc41e2f854f5e43
Joydip <joydip.mondal@gmail.com>
2025-04-17 09:40:58 +0530

    task uploaded

---
+++ codes/day-4/DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DelegateDemo
+{
+    internal class Program
+    {
+        //filter ONLY even numbers from the list and create a new list with those even numbers
+        static void Filter()
+        {
+            //create a new empty list
+            //iterate through the input list using foreach loop
+            //check whether the number is even or not
+            //if even, add the same into the empty list
+
+            //return the list containing even numbers
+        }
+        static void Main()
+        {
+             //create a list containing numbers from 0 ->9
+             //call the filter function and pass this list, which will return the new list containing only even numbers
+             //display all the even numbers from that list
+        }
+    }
+}
