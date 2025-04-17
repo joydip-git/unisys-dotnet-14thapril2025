45b09acc35d491d67bb6a82065050980f2b45eab
Joydip <joydip.mondal@gmail.com>
2025-04-17 16:57:25 +0530

    DAO update

--- codes/Unisys.DotNet.PmsApp/Unisys.DotNet.PmsApp.DataAccessLayer/Implementations/ProductDao.cs
+++ codes/Unisys.DotNet.PmsApp/Unisys.DotNet.PmsApp.DataAccessLayer/Implementations/ProductDao.cs
@@ -1,10 +1,18 @@
 ﻿using Unisys.DotNet.PmsApp.DataAccessLayer.Abstractions;
 using Unisys.DotNet.PmsApp.Models;
+using Unisys.DotNet.PmsApp.Repository;
 
 namespace Unisys.DotNet.PmsApp.DataAccessLayer.Implementations
 {
-    public class ProductDao : IDaoContract<Product, int>
+    public sealed class ProductDao : IDaoContract<Product, int>
     {
+        private readonly ProductRepository _productRepository;
+
+        public ProductDao(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
         public bool Delete(int uniqueId)
         {
             return false;
@@ -12,12 +20,32 @@ namespace Unisys.DotNet.PmsApp.DataAccessLayer.Implementations
 
         public Product? Get(int uniqueId)
         {
-            return null;
+            try
+            {
+                var products = _productRepository.Products;
+                //Func<Product, bool> checkDel = p => p.Id == uniqueId;
+                bool exists = products.Any(p => p.Id == uniqueId);
+                if (exists)
+                    return products.Where(p => p.Id == uniqueId).First();
+                else
+                    return null;
+            }
+            catch (Exception) 
+            { 
+                throw;
+            }
         }
 
         public IEnumerable<Product> GetAll()
         {
-            return [];
+            try
+            {
+                return _productRepository.Products;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public bool Insert(Product entity)
